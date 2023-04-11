e7ece5c171d9b9ded74feebc585658004871d81e
Luis Zubia <soyzubia@gmail.com>
2023-04-11 16:33:54 -0700

    Dragon render the list of dragons Ids && Add Loading conditional message && Add Error conditional message

--- src/components/Dragons.js
+++ src/components/Dragons.js
@@ -1,5 +1,29 @@
-const Dragons = () => (
-  <div>Dragons</div>
-);
+import { useSelector } from 'react-redux';
+import { selectAllDragonsIds, selectError, selectIsLoading } from '../redux/dragons/dragonsSlice';
+
+const Dragons = () => {
+  const dragonsIds = useSelector(selectAllDragonsIds);
+  const dragonsIsLoading = useSelector(selectIsLoading);
+  const errorOcurred = useSelector(selectError);
+
+  if (dragonsIsLoading) {
+    return <div><p>Loading Dragons...</p></div>;
+  }
+
+  return (
+    <section className="dragons-section">
+      { dragonsIds.map((dragonId) => <article key={dragonId}>{dragonId}</article>)}
+      { errorOcurred && (
+      <div>
+        <h3>Something went wrong.</h3>
+        <p>
+          <span>Error:</span>
+          {errorOcurred}
+        </p>
+      </div>
+      )}
+    </section>
+  );
+};
 
 export default Dragons;
