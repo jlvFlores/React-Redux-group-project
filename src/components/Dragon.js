71fcd2cca0482896bf57710c6d6f3327d6b731da
Luis Zubia <soyzubia@gmail.com>
2023-04-11 17:57:28 -0700

    Create Dragon component to be rendered in the list of Dragons

--- src/components/Dragon.js
+++ src/components/Dragon.js
@@ -1,13 +1,28 @@
 import PropTypes from 'prop-types';
+import { useSelector } from 'react-redux';
+import { selectDragonById } from '../redux/dragons/dragonsSlice';
 
 const Dragon = ({ id }) => {
-  console.log(id);
+  const [dragon] = useSelector((store) => selectDragonById(store, id));
+  const { name, description, image } = dragon;
 
-  return (<article>{id}</article>);
+  return (
+    <article className="rocket-card">
+      <img
+        className="rocket-image"
+        src={image}
+        alt={name}
+      />
+      <div className="rocket-info">
+        <h2>{name}</h2>
+        <p>{description}</p>
+      </div>
+    </article>
+  );
 };
 
 Dragon.propTypes = {
-  id: PropTypes.number.isRequired,
+  id: PropTypes.string.isRequired,
 };
 
 export default Dragon;
