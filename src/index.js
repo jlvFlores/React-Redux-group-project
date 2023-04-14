08f45751b70c2debd9dd05ef565f36746cb52d09
Luis Zubia <soyzubia@gmail.com>
2023-04-13 17:17:03 -0700

    Import the new setupStore to use it by the Provider

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import store from './redux/store';
+import setupStore from './redux/store';
 import App from './App';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
+    <Provider store={setupStore()}>
       <App />
     </Provider>
   </React.StrictMode>,
